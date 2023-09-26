f4144257af030061b7e8b8885aacfad374d7bf55
peterelia22 <peterelia914@gmail.com>
2023-09-26 21:34:19 +0300

    add hive

---
+++ lib/Models/NoteModel.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String tittle;
+  final String content;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.tittle,
+      required this.content,
+      required this.date,
+      required this.color});
+}
