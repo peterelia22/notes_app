030d6380766a53331b0e94b9ed062359cec2bd18
peterelia22 <peterelia914@gmail.com>
2023-09-27 21:22:55 +0300

    add note state

--- lib/cubits/addnote/add_notes_cubit.dart
+++ lib/cubits/addnote/add_notes_cubit.dart
@@ -3,6 +3,6 @@ import 'package:meta/meta.dart';
 
 part 'add_notes_state.dart';
 
-class NotesCubit extends Cubit<NotesState> {
-  NotesCubit() : super(NotesInitial());
+class AddNotesCubit extends Cubit<AddNotesState> {
+  AddNotesCubit() : super(AddNotesInitial());
 }
--- lib/cubits/addnote/add_notes_state.dart
+++ lib/cubits/addnote/add_notes_state.dart
@@ -1,6 +1,16 @@
 part of 'add_notes_cubit.dart';
 
 @immutable
-sealed class NotesState {}
+sealed class AddNotesState {}
 
-final class NotesInitial extends NotesState {}
+final class AddNotesInitial extends AddNotesState {}
+
+final class AddNotesLoading extends AddNotesState {}
+
+final class AddNotesSuccess extends AddNotesState {}
+
+final class AddNotesFailure extends AddNotesState {
+  final String errorMessage;
+
+  AddNotesFailure(this.errorMessage);
+}
