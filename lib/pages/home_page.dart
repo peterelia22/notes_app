33c8bec755ca73f307753fd7b5c84205d56e17db
peterelia22 <peterelia914@gmail.com>
2023-10-01 00:41:44 +0300

    fix  some issues

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,6 +12,7 @@ class NotesPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+              isScrollControlled: true,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(25)),
               context: context,
