c395abc4f57c0e0979bb0dc8b449196c6bfa9c39
peterelia22 <peterelia914@gmail.com>
2023-10-04 21:13:19 +0300

    create colors list view part one

--- lib/widgets/AddNoteForm.dart
+++ lib/widgets/AddNoteForm.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:notesapp/cubits/addnote/add_notes_cubit.dart';
+import 'package:notesapp/widgets/Colors_list.dart';
 import 'package:notesapp/widgets/Custom_Button.dart';
 import 'package:notesapp/widgets/Custom_Textfield.dart';
 
@@ -49,22 +50,17 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 40,
           ),
+          ColorsListView(),
+          SizedBox(
+            height: 10,
+          ),
           BlocBuilder<AddNotesCubit, AddNotesState>(
             builder: (context, state) {
               return CustomButton(
                 isLoading: state is AddNotesLoading ? true : false,
                 onTap: () {
                   if (formkey.currentState!.validate()) {
-                    formkey.currentState!.save();
-                    var cuurentDate = DateTime.now();
-                    var formatCuurentDate =
-                        DateFormat('yyyy-MM-dd').format(cuurentDate);
-                    var noteModel = NoteModel(
-                        tittle: tittle!,
-                        content: subTittle!,
-                        date: formatCuurentDate,
-                        color: Colors.blue.value);
-                    BlocProvider.of<AddNotesCubit>(context).AddNote(noteModel);
+                    formatNoteModel(context);
                   } else {
                     autovalidateMode = AutovalidateMode.always;
                     setState(() {});
@@ -76,5 +72,17 @@ class _AddNoteFormState extends State<AddNoteForm> {
         ],
       ),
     );
+  }
+
+  void formatNoteModel(BuildContext context) {
+    formkey.currentState!.save();
+    var cuurentDate = DateTime.now();
+    var formatCuurentDate = DateFormat('yyyy-MM-dd').format(cuurentDate);
+    var noteModel = NoteModel(
+        tittle: tittle!,
+        content: subTittle!,
+        date: formatCuurentDate,
+        color: Colors.blue.value);
+    BlocProvider.of<AddNotesCubit>(context).AddNote(noteModel);
   }
 }
---
+++ lib/widgets/Colors_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircleAvatar(
+      radius: 32,
+      backgroundColor: Colors.black,
+    );
+  }
+}
+
+class ColorsListView extends StatelessWidget {
+  const ColorsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 32 * 2,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return const ColorItem();
+          }),
+    );
+  }
+}
